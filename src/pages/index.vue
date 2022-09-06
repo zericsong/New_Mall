be584f83c329c24e0a11967f7fcd9582cc3933e0
imzeric <zericsong@gmail.com>
2022-09-06 11:29:56 +0800

    setup语法糖和组合式api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        后台首页
+        后台首页 
+        <el-button @click="addCount">{{ count }}</el-button>
+        <el-button type="primary" @click="addCount2">{{ form.count }}</el-button>
+        <!-- <hello-world/> -->
     </div>
 </template>
+<script setup>
+    import { ref,reactive } from "vue"
+    // import HelloWorld from '~/components/HelloWorld.vue'
+
+    let count = ref(1)
+
+    function addCount(){
+        console.log("addCount");
+        count.value++
+        console.log(count.value);
+    }
+
+    const form = reactive({
+        count:2
+    })
+
+    function addCount2(){
+        form.count++
+        console.log(form.count);
+    }
+</script>
