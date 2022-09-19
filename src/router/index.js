a8e829e91474926da3a6c80c68d9ceec4e1857da
imzeric <zericsong@gmail.com>
2022-09-19 19:52:21 +0800

    根据菜单动态添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { 
+import {
     createRouter,
     createWebHashHistory
 } from 'vue-router'
@@ -10,46 +10,74 @@ import NotFound from '~/pages/404.vue'
 import GoodList from '~/pages/goods/list.vue'
 import CategoryList from '~/pages/category/list.vue'
 
+
+// 默认路由，所有用户共享
 const routes = [
     {
-        path:"/",
-        component:Admin,
-        // 子路由
-        children:[{
-            path:"/",
-            component:Index,
-            meta:{
-                title:"后台首页"
-            }
-        },{
-            path:"/goods/list",
-            component:GoodList,
-            meta:{
-                title:"商品管理"
-            }
-        },{
-            path:"/category/list",
-            component:CategoryList,
-            meta:{
-                title:"分类列表"
-            }
-        }]
+        path: "/",
+        name:"admin",
+        component: Admin,
     },
-{
-    path:"/login",
-    component:Login,
+    {
+        path: "/login",
+        component: Login,
+        meta: {
+            title: "登录页"
+        }
+    }, {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound
+    }]
+
+
+// 动态路由，用于匹配菜单动态添加路由
+const asyncRoutes = [{
+    path:"/",
+    name:"/",
+    component:Index,
     meta:{
-        title:"登录页"
+        title:"后台首页"
     }
-},{ 
-    path: '/:pathMatch(.*)*', 
-    name: 'NotFound', 
-    component: NotFound 
+},{
+    path:"/goods/list",
+    name:"/goods/list",
+    component:GoodList,
+    meta:{
+        title:"商品管理"
+    }
+},{
+    path:"/category/list",
+    name:"/category/list",
+    component:CategoryList,
+    meta:{
+        title:"分类列表"
+    }
 }]
 
-const router = createRouter({
-    history:createWebHashHistory(),
+export const router = createRouter({
+    history: createWebHashHistory(),
     routes
 })
 
-export default router
+// 动态添加路由的方法
+export function addRoutes(menus){
+    // 是否有新的路由
+    let hasNewRoutes = false
+    const findAndAddRoutesByMenus = (arr) =>{
+        arr.forEach(e=>{
+            let item = asyncRoutes.find(o=>o.path == e.frontpath)
+            if(item && !router.hasRoute(item.path)){
+                router.addRoute("admin",item)
+                hasNewRoutes = true
+            }
+            if(e.child && e.child.length > 0){
+                findAndAddRoutesByMenus(e.child)
+            }
+        })
+    }
+
+    findAndAddRoutesByMenus(menus)
+
+    return hasNewRoutes
+}
