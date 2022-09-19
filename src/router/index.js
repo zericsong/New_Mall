b501129c3f365fab333582d027138fa3a0002588
imzeric <zericsong@gmail.com>
2022-09-19 19:49:47 +0800

    侧边菜单开发-样式布局和路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Admin from "~/layouts/admin.vue";
 import Index from '~/pages/index.vue'
 import Login from '~/pages/login.vue'
 import NotFound from '~/pages/404.vue'
+import GoodList from '~/pages/goods/list.vue'
 
 const routes = [
     {
@@ -18,6 +19,12 @@ const routes = [
             component:Index,
             meta:{
                 title:"后台首页"
+            }
+        },{
+            path:"/goods/list",
+            component:GoodList,
+            meta:{
+                title:"商品管理"
             }
         }]
     },
