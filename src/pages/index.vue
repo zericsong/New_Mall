2d45d176497c7bcdbc5b05c6065898e4be5bd334
imzeric <zericsong@gmail.com>
2022-09-26 18:02:22 +0800

    统计面板组件开发（二）-骨架屏优化体验

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
         <el-row :gutter="20">
-            <el-col :span="6" :offset="0" v-for="(item,index) in panels" :key="index">
+            <template v-if="panels.length == 0">
+                <el-col :span="6" v-for="i in 4" :key="i">
+                    <el-skeleton style="width: 100%;" animated loading>
+                        <template #template>
+                            <el-card shadow="hover" class="border-0">
+                                <template #header>
+                                    <div class="flex justify-between">
+                                        <el-skeleton-item variant="text" style="width: 50%" />
+                                        <el-skeleton-item variant="text" style="width: 10%" />
+                                    </div>
+                                </template>
+                                <el-skeleton-item variant="h3" style="width: 80%" />
+                                <el-divider />
+                                <div class="flex justify-between text-sm text-gray-500">
+                                    <el-skeleton-item variant="text" style="width: 50%" />
+                                    <el-skeleton-item variant="text" style="width: 10%" />
+                                </div>
+                            </el-card>
+                        </template>
+                    </el-skeleton>
+                </el-col>
+            </template>
+
+
+            <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
                 <el-card shadow="hover" class="border-0">
                     <template #header>
                         <div class="flex justify-between">
                             <span class="text-sm">{{ item.title }}</span>
-                            <el-tag
-                                :type="item.unitColor"
-                                effect="plain"
-                                >
+                            <el-tag :type="item.unitColor" effect="plain">
                                 {{ item.unit }}
                             </el-tag>
                         </div>
@@ -23,20 +44,20 @@
                         <span>{{ item.subValue }}</span>
                     </div>
                 </el-card>
-                
+
             </el-col>
         </el-row>
     </div>
 </template>
 <script setup>
-    import { ref } from "vue"
-    import {
-        getStatistics1
-    } from "~/api/index.js"
+import { ref } from "vue"
+import {
+    getStatistics1
+} from "~/api/index.js"
 
-    const panels = ref([])
-    getStatistics1()
-    .then(res=>{
+const panels = ref([])
+getStatistics1()
+    .then(res => {
         panels.value = res.panels
         console.log(panels.value);
     })
