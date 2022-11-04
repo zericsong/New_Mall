3931a24beca3be4e0657eb6465849d84a4e94d51
erleby <zericsong@gmail.com>
2022-11-04 17:41:45 +0800

    echarts图表组件开发和交互（二）

--- src/components/IndexChart.vue
+++ src/components/IndexChart.vue
@@ -4,66 +4,89 @@
             <div class="flex justify-between">
                 <span class="text-sm">订单统计</span>
                 <div>
-                    <el-check-tag v-for="(item,index) in options" :key="index" :checked="current == item.value" style="margin-right: 8px" @click="handleChoose(item.value)">{{ item.text }}</el-check-tag>
+                    <el-check-tag v-for="(item, index) in options" :key="index" :checked="current == item.value"
+                        style="margin-right: 8px" @click="handleChoose(item.value)">{{ item.text }}</el-check-tag>
                 </div>
             </div>
         </template>
-        <div id="chart" style="width: 100%;height: 300px;"></div>
+        <div ref="el" id="chart" style="width: 100%;height: 300px;"></div>
     </el-card>
 </template>
 <script setup>
-import { ref,onMounted } from 'vue';
+import { ref, onMounted,onBeforeUnmount } from 'vue';
 import * as echarts from 'echarts';
+import { useResizeObserver } from '@vueuse/core'
 
+import {
+    getStatistics3
+} from "~/api/index.js"
 
 const current = ref("week")
 const options = [{
-    text:"近1个月",
-    value:"month"
-},{
-    text:"近1周",
-    value:"week"
-},{
-    text:"近24小时",
-    value:"hour"
+    text: "近1个月",
+    value: "month"
+}, {
+    text: "近1周",
+    value: "week"
+}, {
+    text: "近24小时",
+    value: "hour"
 }]
 
-const handleChoose = (type)=>{
+const handleChoose = (type) => {
     current.value = type
+    getData()
 }
 
 var myChart = null
-onMounted(()=>{
+onMounted(() => {
     var chartDom = document.getElementById('chart');
     myChart = echarts.init(chartDom);
 
     getData()
 })
 
-function getData(){
-    var option;
+onBeforeUnmount(()=>{
+    if(myChart) echarts.dispose(myChart)
+})
 
-    option = {
-    xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [
-        {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar',
-        showBackground: true,
-        backgroundStyle: {
-            color: 'rgba(180, 180, 180, 0.2)'
-        }
-        }
-    ]
+function getData() {
+    let option = {
+        xAxis: {
+            type: 'category',
+            data: []
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: [],
+                type: 'bar',
+                showBackground: true,
+                backgroundStyle: {
+                    color: 'rgba(180, 180, 180, 0.2)'
+                }
+            }
+        ]
     };
 
-    option && myChart.setOption(option);
+    myChart.showLoading()
+    getStatistics3(current.value).then(res=>{
+        option.xAxis.data = res.x
+        option.series[0].data = res.y
+
+        myChart.setOption(option)
+    }).finally(()=>{
+        myChart.hideLoading()
+    })
+
+
+
 }
+
+
+const el = ref(null)
+useResizeObserver(el, (entries) => myChart.resize())
 
 </script>
